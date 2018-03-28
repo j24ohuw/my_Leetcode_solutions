508051e5ab694834ac223b5a342546ad773866f5
John Oh <j24oh.uw@gmail.com>
2018-03-27 23:26:01 -0400

    Update connected_matrix.py

--- connected_matrix.py
+++ connected_matrix.py
@@ -1,16 +1,16 @@
 #https://www.hackerrank.com/challenges/ctci-connected-cell-in-a-grid/problem
 
-
 def getBiggestRegion(grid):
     height = len(grid)
     width = len(grid[0])
-    max_record = 0
+    record = 0
+    
     for h in range(height):
         for w in range(width):
-            if grid[h][w] == 1:
-                max_record = max(DFS(grid,h,w), max_record)
-    return max_record
-
+            if grid[h][w]:
+                record = max(record, DFS(grid,h,w))
+    return record
+    
 def DFS(grid,y,x):
     height = len(grid)
     width = len(grid[0])
@@ -18,19 +18,28 @@ def DFS(grid,y,x):
     grid[y][x] = 0
     
     adjacents = [
-    [y-1,x-1],
-    [y-1,x],
-    [y-1,x+1],
-    [y,x-1],
-    [y,x+1],
-    [y+1,x-1],
-    [y+1,x],
-    [y+1,x+1]]
+        [y+1, x-1],
+        [y+1, x  ],
+        [y+1, x+1],
+        [y  , x-1],
+        [y  , x+1],
+        [y-1, x-1],
+        [y-1, x  ],
+        [y-1, x+1],
+    ]
     
     for adj in adjacents:
-        h = adj[0]
-        w = adj[1]
-        if (adj[0] >= 0 and adj[0] < height) and (adj[1] >= 0 and adj[1] < width):
+        if (adj[0] < height and adj[0] >= 0) and (adj[1] < width and adj[1] >= 0):
+            h = adj[0]
+            w = adj[1]
             if grid[h][w]:
-                count += DFS(grid, h,w)
+                count += DFS(grid, h, w)
     return count
+
+n = int(input().strip())
+m = int(input().strip())
+grid = []
+for grid_i in range(n):
+    grid_t = list(map(int, input().strip().split(' ')))
+    grid.append(grid_t)
+print(getBiggestRegion(grid))
