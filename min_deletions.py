71ba6333f5711b3708ce14f490ecdd3b7fa6d370
John Oh <j24oh.uw@gmail.com>
2018-04-05 16:19:03 -0400

    Update min_deletions.py
    more efficient algo

--- min_deletions.py
+++ min_deletions.py
@@ -1,11 +1,22 @@
 #https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
 
 def number_needed(a, b):
-    intersection = set(a).intersection(b)
-    dict_c = {char:min(a.count(char), b.count(char)) for char in intersection}
-    return len(a) + len(b) - 2*sum(dict_c.values())
+    set_a = build_set(a)
+    set_b = build_set(b)
+    intersection = ''
     
-a = input().strip()
-b = input().strip()
-
-print(number_needed(a, b))
+    for key,val in set_a.items():
+        if set_b.get(key,0) != 0:
+            min_val = min(val,set_b[key])
+            intersection += key*min_val
+    return len(a)+len(b) - 2*len(intersection) 
+    
+    
+def build_set(a):
+    set_a = {}
+    for i in a:
+        if set_a.get(i,0) == 0:
+            set_a[i] = 1
+        else:
+            set_a[i] += 1
+    return set_a
